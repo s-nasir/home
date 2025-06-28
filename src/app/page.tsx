36575de70c963ae400375dc3e8470f74ccf34529
s-nasir <snasirh09@gmail.com>
2025-06-28 13:10:15 -0400

    fixed socials button opacity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,26 +37,30 @@ export default function HomePage() {
         >
           <main className="h-full overflow-y-auto snap-y snap-mandatory scrollbar-hide scroll-smooth">
             <Nav />
+           
 
             <section id="profile" className="h-screen w-full snap-start">
               <div className="flex justify-center items-center h-full relative">
                 <div className="group relative w-[80rem] h-[35rem] mt-[2rem] rounded-lg overflow-hidde transform transition-transform duration-800 hover:scale-60">
-                <div className="absolute inset-0 bg-topograph transition-all duration-500 group-hover:opacity-40 group-hover:scale-105 z-0 transform" />
+                <div className="absolute inset-0 bg-topograph transition-all duration-500 group-hover:opacity-70 group-hover:scale-105 z-0 transform" />
+            
                 <div className="relative z-10 ">
                   <div className="flex justify-center w-[8rem] h-[3rem] bg-black mt-5">
                     <h3 className="text-2xl mt-2 text-white">PROFILE</h3>
                   </div>
-                    <div>
-                      <SocialsSection />
-                    </div>
+        
 
-                  
-                  <div className="p-10 ml-20 mt-20 space-y-5">
+                  <div className="grid grid-flow-col grid-rows-2 gap-1">
+                  <div className="row-span-3 p-10 ml-20 mt-20 space-y-5">
                     <h1 className="text-8xl font-oswald font-semibold">Syed Nasir</h1>
                     <h3 className="text-5xl font-barlow">Software Engineer</h3>
                     <p className="text-l">Toronto, ON</p>
                   </div>
-
+                   <div className="col-span-2 p-5 mt-[8rem] ml-[2rem]">
+                  <SocialsSection />
+                </div>
+                  </div>
+                      
                 </div>
                 </div>
               </div>
--- src/components/ui/socialbutton.tsx
+++ src/components/ui/socialbutton.tsx
@@ -13,21 +13,22 @@ const SocialButton: React.FC<SocialButtonProps> = ({ link, icon, iconFilled, alt
       href={link}
       target="_blank"
       rel="noopener noreferrer"
-      className="group w-10 h-10 bg-[#2F756F] rounded-md flex justify-center items-center transition-all duration-300 hover:opacity-20"
+      className="group relative w-10 h-10 rounded-md flex justify-center items-center transition-transform duration-300 hover:scale-110"
     >
+      <div className="absolute inset-0 bg-[#2F756F] rounded-md transition-opacity duration-300 group-hover:opacity-30"></div>
+
       <img
         src={icon}
         alt={`${alt} icon`}
-        className="absolute w-[30px] h-[30px] transition-opacity duration-300 opacity-100 group-hover:opacity-0"
+        className="absolute w-[30px] h-[30px] transition-opacity duration-300 opacity-100 group-hover:opacity-0 z-10"
       />
       <img
         src={iconFilled}
         alt={`${alt} filled icon`}
-        className="absolute w-[30px] h-[30px] transition-opacity duration-300 opacity-0 group-hover:opacity-100"
+        className="absolute w-[30px] h-[30px] transition-opacity duration-300 opacity-0 group-hover:opacity-100 z-10"
       />
     </a>
   );
 };
 
 export default SocialButton;
-
